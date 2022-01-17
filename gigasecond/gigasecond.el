272e772337e5a1e57601fa25c53e49b5d471f503
zcjava <zcjava@qq.com>
2022-01-17 18:35:14 +0800

    resolve gigasecond subject

--- gigasecond/gigasecond.el
+++ gigasecond/gigasecond.el
@@ -8,12 +8,14 @@
 ;; in the encode-time and decode-time functions.
 
 ;;; Code:
+(require 'seq)
 
-
-
-
-
-
+(defun from (s m h d mon y)
+  (let ((tm (encode-time s m h d mon y "UTC-8")))
+    ;;    (seq-take (decode-time (+ (time-convert tm 'integer) (expt 10 9))) 6)
+    (seq-take (decode-time (time-add tm (expt 10 9)) "UTC-8") 6)
+    )
+  )
 
 (provide 'gigasecond)
 ;;; gigasecond.el ends here
